90061d2d692012c9ced1c4a79af064e9a76a49a3
Bulbul Ahmed <bulbul.official@gmail.com>
2024-05-30 01:51:42 +0600

    SRP commit - 2024-05-30

--- SOLID/SOLID/Program.cs
+++ SOLID/SOLID/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SOLID.SRP;
+
 Console.WriteLine("Hello, World!");
+
+// SRP violation try
+
---
+++ SOLID/SOLID/SRP/ViolateModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.SRP
+{
+    internal class Square
+    {
+        public int length;
+        public Square(int length)
+        {
+            this.length = length;
+        }
+    }
+
+    internal class Circle
+    {
+        public decimal radius;
+
+        public Circle(decimal radius)
+        {
+            this.radius = radius;
+        }
+    }
+
+    internal class AreaCalculator
+    {
+        public decimal[] shape;
+        public AreaCalculator(decimal[] shape)
+        {
+            this.shape = shape;
+        }
+    }
+}
